96e7487804d5adebed4a0a3b263be9e8a9292e1d
David White <dwxw79@gmail.com>
2025-12-01 22:26:45 +0000

    day 1

--- src/day1/day1.test.ts
+++ src/day1/day1.test.ts
@@ -1,6 +1,41 @@
 import { expect } from "@std/expect";
-import { day1 } from "./day1.ts";
+import { day1, day1Part2 } from "./day1.ts";
 
-Deno.test("example", () => {
-  expect(day1(["3   4", "4   3", "2   5", "1   3", "3   9", "3   3"])).toBe(11);
+const test = Deno.test;
+
+test("example", () => {
+  expect(day1([
+    "L68",
+    "L30",
+    "R48",
+    "L5",
+    "R60",
+    "L55",
+    "L1",
+    "L99",
+    "R14",
+    "L82",
+  ])).toBe(3);
+});
+
+test("example", () => {
+  expect(day1Part2([
+    "L68",
+    "L30",
+    "R48",
+    "L5",
+    "R60",
+    "L55",
+    "L1",
+    "L99",
+    "R14",
+    "L82",
+  ])).toBe(6);
+});
+
+test("example long", () => {
+  expect(day1Part2([
+    "L168",
+    "R200",
+  ])).toBe(4);
 });
--- src/day1/day1.ts
+++ src/day1/day1.ts
@@ -1,3 +1,48 @@
-export function day1(input: string[]) {}
+export function day1(input: string[]) {
+  const rotations: [string, number][] = input.map((
+    v,
+  ) => [v[0], parseInt(v.slice(1))]);
+  let dial = 50;
+  let zeros = 0;
+  rotations.forEach(([direction, distance]) => {
+    if (direction === "L") {
+      dial -= distance;
+    } else if (direction === "R") {
+      dial += distance;
+    }
+    dial %= 100;
+    if (dial === 0) {
+      zeros++;
+    }
+  });
+  return zeros;
+}
 
-export function day1Part2(input: string[]) {}
+export function day1Part2(input: string[]) {
+  const rotations: [string, number][] = input.map((
+    v,
+  ) => [v[0], parseInt(v.slice(1))]);
+  let dial = 50;
+  let zeros = 0;
+  rotations.forEach(([direction, distance]) => {
+    if (direction === "L") {
+      if (dial && (dial - distance) < 0) {
+        const r = ((distance) / 100) + 1;
+        const r1 = Math.floor(r);
+        const r2 = Math.abs(r1);
+        zeros += r2;
+      }
+      dial -= distance;
+    } else if (direction === "R") {
+      if (dial && (dial + distance) > 100) {
+        zeros += Math.abs(Math.floor((distance) / 100)) + 1;
+      }
+      dial += distance;
+    }
+    dial = ((dial % 100) + 100) % 100;
+    if (dial === 0) {
+      zeros++;
+    }
+  });
+  return zeros;
+}
